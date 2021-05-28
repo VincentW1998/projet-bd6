a0859e3db6638689031f8e8c7b8e907b15d5d71b
Vincent Wei <vincent.wei@etu.univ-paris-diderot.fr>
2021-05-28 13:37:43 +0200

    quelques requetes 3.0

--- SQL/test.sql
+++ SQL/test.sql
@@ -2,32 +2,41 @@
 SELECT min(date_naissance) 
 FROM client;
 
-\! echo "Requete 2 : la plus jeune date de naissance";
+\! echo "Requete 1.5 : la plus jeune date de naissance";
 SELECT max(date_naissance)
 FROM client;
 
-\! echo "Requete 3 : la personne qui a depensé la plus grosse somme d'argent";
--- SELECT nom, prix
--- FROM achat JOIN commande ON (achat.id_commande = commande.id_commande) 
---     JOIN client ON (commande.id_client = client.id_client)
--- WHERE min(prix);
+\! echo "Requete 2 : la personne qui a depensé la plus grosse somme d'argent";
+SELECT nom, SUM(prix) AS max
+FROM achat NATURAL JOIN commande NATURAL JOIN client
+GROUP BY nom
+ORDER BY max DESC
+LIMIT 1;
 
+\! echo "Requete 2.5 : la personne qui a depensé la plus petite somme d'argent";
+SELECT nom, SUM(prix) AS min
+FROM achat NATURAL JOIN commande NATURAL JOIN client
+GROUP BY nom
+ORDER BY min ASC 
+LIMIT 1;
 
-\! echo "Requete 4 : la personne qui a depensé la plus petite somme d'argent";
-
-\! echo "Requete 5 : la somme du prix depense lors d'achats pour chaque client";
-SELECT nom, SUM(prix) as somme_depense 
-FROM achat JOIN commande ON (achat.id_commande = commande.id_commande) 
-    JOIN client ON (commande.id_client = client.id_client)
+\! echo "Requete 3 : la somme du prix depense lors d'achats pour chaque client";
+SELECT nom, SUM(prix) as somme_depense
+FROM achat NATURAL JOIN commande NATURAL JOIN client
 GROUP BY nom;
 
-\! echo "Requete 6 : le prix d'achat moyen sur le site ";
 
-
-
-\! echo "Requete 7 : la liste des clients qui ont acheté plus de 10 produits";
+\! echo "Requete 4 : la liste des clients qui ont acheté plus de 10 produits";
 SELECT nom, prenom
 FROM client NATURAL JOIN commande NATURAL JOIN achat
 GROUP BY nom, prenom
 HAVING count(id_produit) >= 10;
 
+\! echo "Requete 5 : le prix d'achat moyen sur le site ";
+SELECT ROUND(CAST(AVG(prix) AS NUMERIC), 2) FROM achat;
+
+\! echo "Requete 6 : le chiffre d'affaire par mois toutes annees confondues sur le site ";
+SELECT cast(extract (MONTH FROM date_commande) AS int) AS Mois, sum(prix)
+FROM commande NATURAL JOIN achat 
+GROUP BY Mois 
+ORDER BY Mois;
